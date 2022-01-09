b57eacf61e5eefdac17e61c8204c2ec7d505f59f
Muhammad Eid <39006091+muhammad-eid@users.noreply.github.com>
2022-01-09 16:39:16 +0200

    Delete backend directory

--- backend/routes/profile-route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const profileController = require('../controllers/profile-controller');
-
-const router = express.Router();
-
-
-router.get('/:pid', profileController.getProfileID)
-
-module.exports = router;
